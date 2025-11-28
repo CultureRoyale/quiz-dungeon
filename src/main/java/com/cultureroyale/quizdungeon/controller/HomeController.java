2fe541058f4642d85e0aea970c10660a66e55efb
Slyfti <titout24@outlook.fr>
2025-11-28 14:53:19 +0100

    add supercrawler font

--- src/main/java/com/cultureroyale/quizdungeon/controller/HomeController.java
+++ src/main/java/com/cultureroyale/quizdungeon/controller/HomeController.java
@@ -4,12 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String index(Model model) {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
     }
 }
